bc0b4bd51e126a122a02f06ae864413e745438cb
zSunset <logist26rus@icloud.com>
2023-02-19 02:13:03 +0300

    lesson_6

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
+from authapp import views
 from authapp.apps import AuthappConfig
-
-from . import views
 
 app_name = AuthappConfig.name
 
@@ -10,5 +9,6 @@ urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
+    path("profile_edit/<int:pk>/", views.ProfileEditView.as_view(), name="profile_edit"),
     path("profile_edit/", views.ProfileEditView.as_view(), name="profile_edit"),
 ]
