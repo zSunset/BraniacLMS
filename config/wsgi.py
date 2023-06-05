77c9609f1c2569049ad88f7b3a25d35521e9bc02
zSunset <85899844+zSunset@users.noreply.github.com>
2023-06-05 19:01:47 +0300

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-export DJANGO_SETTINGS_MODULE=nirla.settings
-heroku config:set DJANGO_SETTINGS_MODULE=nirla.settings --account personal
+export DJANGO_SETTINGS_MODULE=config.settings
+heroku config:set DJANGO_SETTINGS_MODULE=config.settings --account personal
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
